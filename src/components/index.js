b06e8c2b97f4309d5afc34d5d17409e6c78e97cf
Willa Shiomos <willashiomos@gmail.com>
2019-11-02 13:49:56 -0400

    expansion panel

--- src/components/index.js
+++ src/components/index.js
@@ -2,9 +2,11 @@ import Modal from './Modal'
 import Button from './Button'
 import Nav from './Nav'
 import RecipeCard from './RecipeCard'
+import ExpansionPanel from './ExpansionPanel'
 
 export {
   Button,
+  ExpansionPanel,
   Modal,
   Nav,
   RecipeCard
