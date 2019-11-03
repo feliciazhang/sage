a8f9d154eb1eae587ff2288a17c38e9dfb6d5b66
Willa Shiomos <willashiomos@gmail.com>
2019-11-03 12:14:17 -0500

    Meal plan (#14)
    * nav + pages + styles
    
    * merge commit
    
    * expansion panel
    
    * font things
    
    * initial commit of adding
    
    * working commit
    
    * delete test plan
    
    * add margins on panels

--- src/components/ExpansionPanel/index.js
+++ src/components/ExpansionPanel/index.js
@@ -1,50 +1,107 @@
-import React from "react"
+import React, { useState } from "react"
 import PropTypes from "prop-types"
 import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io"
+import { Button, Modal, Dropdown, Input, Delete } from "../"
+import { updateMeal } from "../../state/mealPlan"
 import "./style.css"
+import { useDispatch } from "react-redux"
 
-class ExpansionPanel extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { expanded: true }
-    this.expand = this.expand.bind(this)
-    this.close = this.close.bind(this)
+const ExpansionPanel = ({ children, title, meals, recipeOptions }) => {
+  const [expanded, setExpanded] = useState(true)
+  const [adding, setAdding] = useState(false)
+  const [recMeal, setRecMeal] = useState(null)
+  const [recServings, setRecServings] = useState(1)
+  const dispatch = useDispatch()
+
+  const expand = () => {
+    setExpanded(true)
   }
 
-  expand() {
-    this.setState({
-      expanded: true,
+  const close = () => {
+    setExpanded(false)
+  }
+
+  const addMeal = () => {
+    setAdding(false)
+    const newMeals = meals
+    newMeals.push({
+      meal: recMeal,
+      servings: recServings,
     })
+    dispatch(updateMeal(title, newMeals))
   }
 
-  close() {
-    this.setState({
-      expanded: false,
-    })
+  const deleteMeal = index => {
+    const clone = meals
+    clone.splice(index, 1)
+    dispatch(updateMeal(title, clone))
   }
 
-  render() {
-    return (
-      <div className="panel-container">
-        <div className="panel-header">
-          <div className="panel-title">{this.props.title}</div>
-          {this.state.expanded ? (
-            <div className="panel-icon">
-              <IoIosArrowUp size={24} onClick={this.close} />
-            </div>
-          ) : (
-            <div className="panel-icon">
-              <IoIosArrowDown size={24} onClick={this.expand} />
-            </div>
+  return (
+    <div className="panel-container">
+      <div className="panel-header">
+        <div className="panel-title">{title}</div>
+        {expanded ? (
+          <div className="panel-icon">
+            <IoIosArrowUp size={24} onClick={() => close()} />
+          </div>
+        ) : (
+          <div className="panel-icon">
+            <IoIosArrowDown size={24} onClick={() => expand()} />
+          </div>
+        )}
+      </div>
+      {expanded && (
+        <div className="panel-content">
+          <div className="panel-meals">
+            {meals.map((item, index) => (
+              <div className="panel-meal">
+                <div className="meal-name">{item.meal + ": " }</div>
+                  <Input
+                    className="sage-recipe-modal--number servings-input"
+                    type="number"
+                    size="small"
+                    label="Servings: "
+                    value={item.servings}
+                    onChange={val => console.log(val)}
+                  />
+                <div className="panel-delete-meal">
+                  <Delete onClick={() => deleteMeal(index)} />{" "}
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="panel-add">
+            <Button onClick={() => setAdding(true)}>+ Add Meal</Button>
+          </div>
+          {adding && (
+            <Modal
+              onClose={() => setAdding(false)}
+              title={"Add Meal: " + title}
+              isOpen={true}
+            >
+              <div className="panel-input">
+                <Dropdown
+                  placeholder="Pick recipe to add"
+                  options={recipeOptions}
+                  onChange={val => setRecMeal(val)}
+                />
+                <Input
+                  className="sage-recipe-modal--servings"
+                  type="number"
+                  size="small"
+                  label="Servings: "
+                  value={1}
+                  onChange={val => setRecServings(val)}
+                />
+                <Button onClick={() => addMeal()}>Add Meal</Button>
+              </div>
+            </Modal>
           )}
         </div>
-        {this.state.expanded ? (
-          <div className="panel-content">
-          <div className="panel-meals">{this.props.children}</div></div>
-        ) : null}
-      </div>
-    )
-  }
+      )}
+    </div>
+  )
 }
 
 ExpansionPanel.propTypes = {
@@ -56,6 +113,14 @@ ExpansionPanel.propTypes = {
    * The main label that appears on the expansion panel
    */
   title: PropTypes.string.isRequired,
+  /**
+   * The meals data
+   */
+  meals: PropTypes.node.isRequired,
+  /**
+   * The recipes data
+   */
+  recipeOptions: PropTypes.node.isRequired,
 }
 
 export default ExpansionPanel
